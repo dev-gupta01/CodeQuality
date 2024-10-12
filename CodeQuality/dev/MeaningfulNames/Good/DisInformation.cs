e6f444af811d7bb57069100a1d3d48310e25cd85
Devashish Gupta <devagupta@microsoft.com>
2024-10-12 22:27:42 +0530

    meaningfulNames

---
+++ CodeQuality/dev/MeaningfulNames/Good/DisInformation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeQuality.dev.MeaningfulNames.Good
+{
+    internal class DisInformation
+    {
+    }
+}
